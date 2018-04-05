80ae38e268bf4b27fd38204ff4d3483dc526e1d6
Michael Dawson <mdawson@marketplacer.com>
2018-04-05 20:35:23 +1000

    Setup parse controller to parse remote URLs

--- app/controllers/parse_controller.rb
+++ app/controllers/parse_controller.rb
@@ -1,5 +1,9 @@
 class ParseController < ApplicationController
   def parse
-    render body: '123'
+    response = HTTParty.get(params[:url])
+    document = Nokogiri::HTML(response.body)
+    result = document.xpath(params[:xpath])
+
+    render body: result.text.strip
   end
 end
--- spec/controllers/parse_controller_spec.rb
+++ spec/controllers/parse_controller_spec.rb
@@ -7,14 +7,29 @@ RSpec.describe ParseController, type: :controller do
     context 'with a param for a url and an XPath' do
       let!(:params) {
         {
-          url: 'somewebsite.com/data',
-          xpath: '//*[@id="yDmH0d"]/div[4]/div[2]/div/div[2]'
+          url: 'https://somewebsite.com/data',
+          xpath: '/html/body/div/div'
         }
       }
 
-      it 'returns a plain response with the contents of the node described by the XPath' do
-        get :parse
-        expect(response.body).to eq('123')
+      context 'when our website returns data' do
+        let(:stubbed_body) {
+          <<~HTML
+            <html>
+              <body>
+                <div>
+                  <div>I hold the magic data</div>
+                </div>
+              </body>
+            </html>
+          HTML
+        }
+        before { stub_request(:get, params[:url]).to_return(body: stubbed_body) }
+
+        it 'returns a plain response with the contents of the node described by the XPath' do
+          get :parse, params: params
+          expect(response.body).to eq('I hold the magic data')
+        end
       end
     end
   end
